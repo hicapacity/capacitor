8001d66573760c8e7b936faf06504768330027ae
James Wang <jw@slickage.com>
2014-01-30 20:54:33 -1000

    Initial with devise.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capacitor::Application.config.secret_key_base = '2e5fe8c9338021df4753851d6a29e7f4b08944475f4d1bcb40bb0fcae0ad551971635dac81b6c47d495d023695d0f2d6d0fe714f47f8639972edc697f07d7798'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Capacitor::Application.config.session_store :cookie_store, key: '_capacitor_session'
