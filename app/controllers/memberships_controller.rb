2dddd48fa171b9054714c02c415aea98fceaa6e5
Julie Ann Sakuda <jsakuda@gmail.com>
2014-09-29 02:21:39 -1000

    Adding membership route.
    Form for collecting payment information. Not functional.

---
+++ app/controllers/memberships_controller.rb
@@ -0,0 +1,13 @@
+class MembershipsController < ApplicationController
+  def index
+
+  end
+  
+  def show
+
+  end
+  
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Capacitor::Application.routes.draw do
   devise_for :users
   resource :profile
   resources :charges
+  resources :memberships
   root 'home#index'
 end
