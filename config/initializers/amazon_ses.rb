dc210285de69dab5cbb4e01c9df64340bc1d643c
James Wang <jw@slickage.com>
2015-02-27 02:40:56 -1000

    SES for production, development uses gmail
    - gmail fixes
    - :host for config.action_mailer.default_url_options on both prod/dev

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,4 @@
+require 'aws/ses'
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   access_key_id: ENV['AMAZON_ACCESS_KEY'],
   secret_access_key: ENV['AMAZON_SECRET_KEY']
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023102328) do
+ActiveRecord::Schema.define(version: 20141024093025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,11 +45,13 @@ ActiveRecord::Schema.define(version: 20141023102328) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "stripe_customer_id"
+    t.integer  "role_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
   add_index "users", ["stripe_customer_id"], name: "index_users_on_stripe_customer_id", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
