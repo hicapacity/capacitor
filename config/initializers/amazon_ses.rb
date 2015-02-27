c23e5415e055ea52a9eea7bcea3523617e4328c7
James Wang <jw@slickage.com>
2015-02-27 02:58:56 -1000

    Setting default prod source to us-west2

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,5 @@
 require 'aws/ses'
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   access_key_id: ENV['AMAZON_ACCESS_KEY'],
-  secret_access_key: ENV['AMAZON_SECRET_KEY']
+  secret_access_key: ENV['AMAZON_SECRET_KEY'],
+  server: 'email-smtp.us-west-2.amazonaws.com'
